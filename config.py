8ca8f870e391cd0d448f652ac83fb9a375b2a403
Dermot Kelly <mrdermotkelly@gmail.com>
2023-09-20 22:24:51 +0100

    app commit

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY_F')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
